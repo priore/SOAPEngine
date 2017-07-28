788b46e22f1f0246dc8e36cacd4b101fd5e50fb2
Danilo <danilo.priore@gmail.com>
2017-07-28 11:53:26 +0200

    fixes crash's #165

--- SOAPEngineOSX.framework/Versions/A/Headers/SOAPEngine.h
+++ SOAPEngineOSX.framework/Versions/A/Headers/SOAPEngine.h
@@ -10,11 +10,11 @@
 //
 //  email support: support@prioregroup.com
 //
-// Version      : 1.4.2
+// Version      : 1.4.3
 // Changelog    : https://github.com/priore/SOAPEngine/blob/master/CHANGELOG.txt
 // Updates      : https://github.com/priore/SOAPEngine
 //
-#define SOAPEngineFrameworkVersion @"1.4.2" DEPRECATED_MSG_ATTRIBUTE("SOAPEngineFrameworkVersion as deprecated please use SOAPEngine64VersionString")
+#define SOAPEngineFrameworkVersion @"1.4.3" DEPRECATED_MSG_ATTRIBUTE("SOAPEngineFrameworkVersion as deprecated please use SOAPEngine64VersionString")
 
 #import <Foundation/Foundation.h>
 
